5b90c4333391007772039b05b78a06278bb2172a
Zair Rogelio <71991326+zthewriter@users.noreply.github.com>
2023-10-19 01:01:26 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let lastExportedHtml = "";
+
 const exportBillText = async () => {
     const table = document.getElementById('billTable');
     let exportedText = '<html><head><title>Bill Texts</title></head><body>';
@@ -22,6 +24,8 @@ const exportBillText = async () => {
 
     exportedText += '</body></html>';
 
+    lastExportedHtml = exportedText; // Store the exported HTML
+
     const blob = new Blob([exportedText], {type: "text/html"});
     const link = document.createElement("a");
     link.href = URL.createObjectURL(blob);
@@ -30,3 +34,32 @@ const exportBillText = async () => {
 };
 
 document.getElementById('exportBtn').addEventListener('click', exportBillText);
+
+const questions = { 
+    "What is your name?": "I'm the Replit bot.",
+    "What can you do?": "I can help answer questions related with this page.",
+    "What is the Bill Data?": "Bill Data is the data related to specific bills which contains bill number, bill id, text url, last action date, last action information and summary of the bill.",
+    "What does export button do?": "The export button exports the text of all bills in the table into a HTML file.",
+    "Show me the exported text": () => lastExportedHtml // Add a question to show the exported HTML text 
+};
+
+window.onload = () => {
+    const chatIcon = document.createElement("div");
+    chatIcon.innerHTML = "&#x1F5E9;"; // Unicode for chat icon
+    chatIcon.style.position = 'fixed';
+    chatIcon.style.bottom = '20px';
+    chatIcon.style.right = '20px';
+    chatIcon.style.background = '#4CAF50';
+    chatIcon.style.color = 'white';
+    chatIcon.style.padding = '10px';
+    chatIcon.style.borderRadius = '50%';
+    chatIcon.style.cursor = 'pointer';
+    document.body.appendChild(chatIcon);
+
+    chatIcon.addEventListener('click', () => {
+        const question = prompt("What's your question?");
+        const answer = questions[question] || "Sorry, I couldn't understand your question.";
+
+        alert(typeof answer === "function" ? answer() : answer);
+    });
+};
